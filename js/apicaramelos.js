33b279278cb7defad859a94102a34318e0850692
thaisperlho <carvalho.thais0301@gmail.com>
2022-11-18 16:17:48 -0300

    update apicaramelos.js

--- js/apicaramelos.js
+++ js/apicaramelos.js
@@ -5,21 +5,47 @@ let pets;
 document.onreadystatechange = () => {
     if (document.readyState == 'complete') {
         petGetAll();
+        petGetAll2();
     }
 }
 
 function petGetAll() {
-    $("#pet-list").empty();
+    $("#pets-list").empty();
     $.getJSON(url, function(data) {
         pets = data;
-        const ul = $("<ul>");
+        const card = $("<div>");
         pets.forEach(e => {
-            const li = $("<li>");
+            const img = $("<img src=\"img/cao.png\">")
+            const div = $("<div>");
             //const chk = $(`<input type='checkbox' ${(e.status == 'DONE') ? 'checked' : '' } onclick="${(e.status == 'DONE') ? 'markPending(this.value)' : 'markDone(this.value)'}" value=${e.id}>`); 
-            const nome = $(`<label>${e.nome}</label>`);
-            ul.append(li.append(nome));
+            const nome = $(`<h4><b>${e.nome}</b></h4>`);
+            const idade = $(`<p>${e.idade}</p>`)
+            card.append(img,div.append(nome,idade));
         });
-        $("#pets-list").append(ul);
+        $("#pets-list").append(card);
     });
-        
+}  
+
+function petGetAll2() {
+    $("#pet-card").empty();
+    $.getJSON(url, function(data) {
+        pets = data;
+        const b = $(`<div class="row">`)
+        const c = $(`<div class="col">`)
+        const card = $(`<div class="card" style="width: 18rem;">`)
+        for(let i=0; i<3; i++){
+            const img = $(`<img src="img/gato.png" class="card-img-top" alt="...">`)
+            const div = $("<div>");
+            //const chk = $(`<input type='checkbox' ${(e.status == 'DONE') ? 'checked' : '' } onclick="${(e.status == 'DONE') ? 'markPending(this.value)' : 'markDone(this.value)'}" value=${e.id}>`); 
+            const nome = $(`<h5 class="card-title">${pets[i].nome}</h5>`);
+            const idade = $(`<p class="card-text">${pets[i].idade}</p>`)
+            const a = $(`<a href="#" class="btn btn-primary">visualisar</a>`)
+            card.append(img,div.append(nome,idade,a));
+        }
+        for(let i=0; i<3; i++){
+            c.append(card);
+        }
+        $("#pet-card").append(b.append(c));
+    });
 }
+    
